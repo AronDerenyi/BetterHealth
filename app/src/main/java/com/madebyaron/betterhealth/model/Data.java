c09e0af62072980e46a064c1be0120fb9807b34a
AronDerenyi <aron.derenyi@gmail.com>
2019-12-07 01:29:44 +0100

    Converted the Data class to java

--- app/src/main/java/com/madebyaron/betterhealth/model/Data.java
+++ app/src/main/java/com/madebyaron/betterhealth/model/Data.java
@@ -1,13 +1,39 @@
-package com.madebyaron.betterhealth.model
+package com.madebyaron.betterhealth.model;
 
-import java.util.*
+import java.util.*;
 
-data class Data(val id: Int, val type: Int, val date: Date, val amount: Float) {
+public class Data {
 
-	companion object {
-		const val TYPE_WEIGHT = 0
-		const val TYPE_CALORIES = 1
-		const val TYPE_DRINK = 2
-		const val TYPE_CIGARETTES = 3
+	public static final int TYPE_WEIGHT = 0;
+	public static final int TYPE_CALORIES = 1;
+	public static final int TYPE_DRINK = 2;
+	public static final int TYPE_CIGARETTES = 3;
+
+	private final int id;
+	private final int type;
+	private final Date date;
+	private final float amount;
+
+	public Data(int id, int type, Date date, float amount) {
+		this.id = id;
+		this.type = type;
+		this.date = date;
+		this.amount = amount;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public float getAmount() {
+		return amount;
 	}
 }
